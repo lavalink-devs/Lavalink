27c71459d47226b56887470eb2f42d67448c2341
Frederikam <frogkr@gmail.com>
2017-08-04 16:41:11 +0200

    Fixed audio loss counter reporting itself as having insufficient data

--- LavalinkServer/src/main/java/lavalink/server/player/AudioLossCounter.java
+++ LavalinkServer/src/main/java/lavalink/server/player/AudioLossCounter.java
@@ -26,8 +26,12 @@ import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import com.sedmelluq.discord.lavaplayer.player.event.AudioEventAdapter;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrackEndReason;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AudioLossCounter extends AudioEventAdapter {
+
+    private static final Logger log = LoggerFactory.getLogger(AudioLossCounter.class);
 
     public static final int EXPECTED_PACKET_COUNT_PER_MIN = (60 * 1000) / 20; // 20ms packets
     private static final int ACCEPTABLE_TRACK_SWITCH_TIME = 100; //ms
@@ -65,9 +69,11 @@ public class AudioLossCounter extends AudioEventAdapter {
     }
 
     public boolean isDataUsable() {
+        //log.info("\n" + lastTrackStarted + "\n" + lastTrackEnded + "\n" + playingSince);
+
         // Check that there isn't a significant gap in playback. If no track has ended yet, we can look past that
         if(lastTrackStarted - lastTrackEnded > ACCEPTABLE_TRACK_SWITCH_TIME
-                && lastTrackEnded != Long.MAX_VALUE) return false;
+                && lastTrackEnded != Long.MAX_VALUE ) return false;
 
         // Check that we have at least stats for last minute
         long lastMin = System.currentTimeMillis() / 60000 - 1;
@@ -95,8 +101,10 @@ public class AudioLossCounter extends AudioEventAdapter {
     public void onTrackStart(AudioPlayer player, AudioTrack track) {
         lastTrackStarted = System.currentTimeMillis();
 
-        if (lastTrackStarted - lastTrackEnded > ACCEPTABLE_TRACK_SWITCH_TIME) {
+        if (lastTrackStarted - lastTrackEnded > ACCEPTABLE_TRACK_SWITCH_TIME
+                || playingSince == Long.MAX_VALUE) {
             playingSince = System.currentTimeMillis();
+            lastTrackEnded = Long.MAX_VALUE;
         }
     }
 
