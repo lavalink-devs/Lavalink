1b1cf07271727968e3b2df6766e1c6da4a3d61ce
Fabricio20 <fabricio20r@hotmail.com>
2017-10-21 13:16:10 -0200

    Javadoc changes

--- LavalinkClient/src/main/java/lavalink/client/io/PenaltyProvider.java
+++ LavalinkClient/src/main/java/lavalink/client/io/PenaltyProvider.java
@@ -25,11 +25,13 @@ package lavalink.client.io;
 public interface PenaltyProvider {
 
     /**
-     * Using the penalties class allows for fetching default penalties like CPU or Players.
+     * This method allows for adding custom penalties to {@link LavalinkSocket nodes}, making it possible to
+     * change how the node selection system works on a per-guild per-node basis.
+     * By using the provided {@link LavalinkLoadBalancer.Penalties Penalties} class you can fetch default penalties like CPU or Players.
      *
-     * @param balancer - Instance of Penalties class representing the node to check.
+     * @param penalties - Instance of {@link LavalinkLoadBalancer.Penalties Penalties} class representing the node to check.
      * @return total penalty to add to this node.
      */
-    int getPenalty(LavalinkLoadBalancer.Penalties balancer);
+    int getPenalty(LavalinkLoadBalancer.Penalties penalties);
 
 }
