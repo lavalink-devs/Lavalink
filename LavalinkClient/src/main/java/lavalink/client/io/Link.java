ca01559ba1226bd1b5c57fc0fe59db5edba2f070
Frederikam <frogkr@gmail.com>
2017-10-29 14:30:09 +0100

    Whoopsie

--- LavalinkClient/src/main/java/lavalink/client/io/Link.java
+++ LavalinkClient/src/main/java/lavalink/client/io/Link.java
@@ -342,7 +342,7 @@ public class Link {
 
     private void setState(State state) {
         if (this.state == State.DESTROYED && state != State.DESTROYED)
-            throw new IllegalStateException("Cannot change state to {} when state is {}", state, State.DESTROYED);
+            throw new IllegalStateException("Cannot change state to " + state + " when state is " + State.DESTROYED);
 
         log.debug("Link {} changed state from {} to {}", this, this.state, state);
         this.state = state;
