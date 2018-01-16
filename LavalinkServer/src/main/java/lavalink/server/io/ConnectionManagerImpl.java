3ed17ac008bca03fd791323d198d9da5e6dac356
Repulser <thetechproject2014@gmail.com>
2018-01-16 22:15:01 +0200

    extra warnings + update JDAA

--- LavalinkServer/src/main/java/lavalink/server/io/ConnectionManagerImpl.java
+++ LavalinkServer/src/main/java/lavalink/server/io/ConnectionManagerImpl.java
@@ -3,13 +3,17 @@ package lavalink.server.io;
 import net.dv8tion.jda.manager.ConnectionManager;
 import org.java_websocket.WebSocket;
 import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by Repulser
  * https://github.com/Repulser
  */
 public class ConnectionManagerImpl implements ConnectionManager {
+    private static final Logger log = LoggerFactory.getLogger(ConnectionManagerImpl.class);
     private final WebSocket socket;
+
 
     ConnectionManagerImpl(WebSocket socket) {
         this.socket = socket;
@@ -21,15 +25,15 @@ public class ConnectionManagerImpl implements ConnectionManager {
 
     @Override
     public void queueAudioConnect(String guildId, String channelId) {
+        log.warn("queueAudioConnect was requested, this shouldn't happen");
     }
 
     @Override
-    public void shouldReconnect(String guildId) {
+    public void onDisconnect(String guildId) {
         JSONObject obj = new JSONObject()
                 .put("op", "disconnected")
                 .put("guildId", guildId);
         socket.send(obj.toString());
     }
-
 
 }
