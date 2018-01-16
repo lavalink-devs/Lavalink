51d3c691dae13693dcf098badbf02078b0f101b5
Repulser <thetechproject2014@gmail.com>
2018-01-16 12:33:56 +0200

    Fixups

--- LavalinkServer/src/main/java/lavalink/server/io/ConnectionManagerImpl.java
+++ LavalinkServer/src/main/java/lavalink/server/io/ConnectionManagerImpl.java
@@ -3,15 +3,12 @@ package lavalink.server.io;
 import net.dv8tion.jda.manager.ConnectionManager;
 import org.java_websocket.WebSocket;
 import org.json.JSONObject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Created by Repulser
  * https://github.com/Repulser
  */
 public class ConnectionManagerImpl implements ConnectionManager {
-    private static final Logger log = LoggerFactory.getLogger(ConnectionManagerImpl.class);
     private final WebSocket socket;
 
     ConnectionManagerImpl(WebSocket socket) {
@@ -20,12 +17,10 @@ public class ConnectionManagerImpl implements ConnectionManager {
 
     @Override
     public void removeAudioConnection(String guildId) {
-        log.warn("removeAudioConnection was requested, this shouldn't happen");
     }
 
     @Override
     public void queueAudioConnect(String guildId, String channelId) {
-
     }
 
     @Override
