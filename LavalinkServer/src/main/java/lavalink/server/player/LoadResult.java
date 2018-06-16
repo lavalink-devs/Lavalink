41b0434e765b88a7b2db063a400f7d0c893560cb
Sam <ophisbonklol@gmail.com>
2018-06-16 23:18:01 +0100

    move load result out into its own class

---
+++ LavalinkServer/src/main/java/lavalink/server/player/LoadResult.java
@@ -0,0 +1,22 @@
+package lavalink.server.player;
+
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+
+import java.util.Collections;
+import java.util.List;
+
+class LoadResult {
+    public List<AudioTrack> tracks;
+    public boolean isPlaylist;
+    public String playlistName;
+    public String loadResultType;
+    public Integer selectedTrack;
+
+    LoadResult(List<AudioTrack> tracks, boolean isPlaylist, String playlistName, ResultStatus loadResultType, Integer selectedTrack) {
+        this.tracks = Collections.unmodifiableList(tracks);
+        this.isPlaylist = isPlaylist;
+        this.playlistName = playlistName;
+        this.loadResultType = loadResultType.name();
+        this.selectedTrack = selectedTrack;
+    }
+}
