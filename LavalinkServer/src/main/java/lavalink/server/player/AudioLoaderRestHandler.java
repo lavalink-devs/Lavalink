671be1b7270ddb674f8cad202b731695564b9a32
Devoxin <lukel9914@gmail.com>
2018-11-09 15:13:11 +0000

    Remove unused import

--- LavalinkServer/src/main/java/lavalink/server/player/AudioLoaderRestHandler.java
+++ LavalinkServer/src/main/java/lavalink/server/player/AudioLoaderRestHandler.java
@@ -31,7 +31,6 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
