adc431b8f32adb9d5914d5100efb55c75e8c95f3
Napster <napster@npstr.space>
2019-03-08 22:50:22 +0100

    Add spring config for tests

--- LavalinkServer/src/test/java/lavalink/server/config/RequestAuthorizationFilterTest.java
+++ LavalinkServer/src/test/java/lavalink/server/config/RequestAuthorizationFilterTest.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -20,6 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
 @SpringBootTest()
+@ActiveProfiles({"test"})
 public class RequestAuthorizationFilterTest {
 
     @Autowired
