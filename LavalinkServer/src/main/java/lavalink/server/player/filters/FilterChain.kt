44c272ee549a8e5ec9b8408b718103c63c25062a
Frederik Ar. Mikkelsen <fred@frederikam.com>
2019-07-31 10:48:14 +0200

    Only build filters we need

--- LavalinkServer/src/main/java/lavalink/server/player/filters/FilterChain.kt
+++ LavalinkServer/src/main/java/lavalink/server/player/filters/FilterChain.kt
@@ -7,10 +7,13 @@ import com.sedmelluq.discord.lavaplayer.filter.PcmFilterFactory
 import com.sedmelluq.discord.lavaplayer.filter.UniversalPcmAudioFilter
 import com.sedmelluq.discord.lavaplayer.format.AudioDataFormat
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 class FilterChain : PcmFilterFactory {
 
     companion object {
+        private val log: Logger = LoggerFactory.getLogger(FilterChain::class.java)
         private val gson = Gson()
         fun parse(json: String) = gson.fromJson(json, FilterChain::class.java)!!
     }
@@ -22,7 +25,18 @@ class FilterChain : PcmFilterFactory {
     private val tremolo: TremoloConfig? = null
     private val vibrato: VibratoConfig? = null
 
-    override fun buildChain(track: AudioTrack, format: AudioDataFormat, output: UniversalPcmAudioFilter): MutableList<AudioFilter> {
+    private fun buildList() = listOfNotNull(
+            volume?.let { VolumeConfig(it) },
+            equalizer?.let { EqualizerConfig(it) },
+            karaoke,
+            timescale,
+            tremolo,
+            vibrato
+    )
+
+    val isEnabled get() = buildList().any { it.isEnabled }
+
+    override fun buildChain(track: AudioTrack?, format: AudioDataFormat, output: UniversalPcmAudioFilter): MutableList<AudioFilter> {
         val list = listOfNotNull(
                 volume?.let { VolumeConfig(it) },
                 equalizer?.let { EqualizerConfig(it) },
@@ -38,6 +52,10 @@ class FilterChain : PcmFilterFactory {
                 .forEach { builder.addFirst(it) }
 
         return builder.build(null, format.channelCount)
-                .filters.apply { dropLast(1) }
+                .filters//.apply { dropLast(1) }
+                .apply {
+                    log.info(this.map { it.javaClass.simpleName }.toString())
+                }
     }
+
 }
