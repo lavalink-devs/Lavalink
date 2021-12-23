66eb298b3f586ad2f9a4786c73672fb932b139c6
Freya Arbjerg <freya@arbjerg.dev>
2021-12-23 16:42:07 +0100

    Convert event handler from interface to abstract class

--- plugin-api/src/main/java/dev/arbjerg/lavalink/api/PluginEventHandler.java
+++ plugin-api/src/main/java/dev/arbjerg/lavalink/api/PluginEventHandler.java
@@ -3,46 +3,46 @@ package dev.arbjerg.lavalink.api;
 /**
  * Must be provided as a bean
  */
-public interface PluginEventHandler {
+public abstract class PluginEventHandler {
 
     /**
      * Fired upon a new WebSocket being opened
      * @param context the websocket
      * @param resumed if the context was resumed and thus reused
      */
-    default void onWebSocketOpen(ISocketContext context, boolean resumed) {}
+    void onWebSocketOpen(ISocketContext context, boolean resumed) {}
 
     /**
      * Fired upon a WebSocket being closed
      * @param context the websocket
      */
-    default void onWebSocketClose(ISocketContext context) {}
+    void onWebSocketClose(ISocketContext context) {}
 
     /**
      * Fired upon a WebSocket message being received
      * @param context the websocket
      * @param message the message, presumably in JSON
      */
-    default void onWebsocketMessageIn(ISocketContext context, String message) {}
+    void onWebsocketMessageIn(ISocketContext context, String message) {}
 
     /**
      * Fired upon a WebSocket message being sent
      * @param context the websocket
      * @param message the message, presumably in JSON
      */
-    default void onWebSocketMessageOut(ISocketContext context, String message) {}
+    void onWebSocketMessageOut(ISocketContext context, String message) {}
 
     /**
      * Fired upon a new player being created
      * @param context the websocket
      * @param player the new player
      */
-    default void onNewPlayer(ISocketContext context, IPlayer player) {}
+    void onNewPlayer(ISocketContext context, IPlayer player) {}
 
     /**
      * Fired upon a player being destroyed
      * @param context the websocket
      * @param player the player to be destroyed
      */
-    default void onDestroyPlayer(ISocketContext context, IPlayer player) {}
+    void onDestroyPlayer(ISocketContext context, IPlayer player) {}
 }
