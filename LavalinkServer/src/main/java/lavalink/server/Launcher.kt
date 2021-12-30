12de4092e4f6458a472e827bc2759e02f540bc2b
Freya Arbjerg <freya@arbjerg.dev>
2021-12-30 13:05:05 +0100

    Fix REST endpoints

--- LavalinkServer/src/main/java/lavalink/server/Launcher.kt
+++ LavalinkServer/src/main/java/lavalink/server/Launcher.kt
@@ -136,6 +136,7 @@ object Launcher {
         val pluginManager = parent.getBean(PluginManager::class.java)
         val properties = Properties()
         properties["componentScan"] = pluginManager.pluginManifests.map { it.path }
+            .toMutableList().apply { add("lavalink.server") }
 
         SpringApplicationBuilder()
             .sources(LavalinkApplication::class.java)
