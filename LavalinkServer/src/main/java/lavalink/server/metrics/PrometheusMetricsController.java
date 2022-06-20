28c1796e476b2cc97b9844f8dfda63a42038c563
melike2d <ml2dofficial@gmail.com>
2022-06-20 12:40:13 -0700

    Revert annotation import change

--- LavalinkServer/src/main/java/lavalink/server/metrics/PrometheusMetricsController.java
+++ LavalinkServer/src/main/java/lavalink/server/metrics/PrometheusMetricsController.java
@@ -2,7 +2,6 @@ package lavalink.server.metrics;
 
 import io.prometheus.client.CollectorRegistry;
 import io.prometheus.client.exporter.common.TextFormat;
-import org.jetbrains.annotations.Nullable;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Nullable;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.io.Writer;
