26a43fa653ee411074cc681bc3b88716de11789e
TopiSenpai <tobi@strasserhof.com>
2022-11-08 23:39:50 +0100

    add deprecation notice to old methods in AudioFilterExtension

--- plugin-api/src/main/java/dev/arbjerg/lavalink/api/AudioFilterExtension.java
+++ plugin-api/src/main/java/dev/arbjerg/lavalink/api/AudioFilterExtension.java
@@ -22,11 +22,23 @@ public interface AudioFilterExtension {
      * @param format format as specified by Lavaplayer.
      * @param output the output to be used by the produced filter.
      * @return a filter which produces the desired audio effect.
+     *
+     * @deprecated As of v3.7 Jackson is the preferred way of JSON serialization,
+     * use {@link AudioFilterExtension#build(JsonNode, AudioDataFormat, FloatPcmAudioFilter)} instead.
      */
     default FloatPcmAudioFilter build(JSONObject data, AudioDataFormat format, FloatPcmAudioFilter output) {
         return null;
     }
 
+    /**
+     * Builds a filter for a particular player.
+     *
+     * @param data   JSON data received from the client under the extension name key.
+     * @param format format as specified by Lavaplayer.
+     * @param output the output to be used by the produced filter.
+     * @return a filter which produces the desired audio effect.
+     *
+     */
     default FloatPcmAudioFilter build(JsonNode data, AudioDataFormat format, FloatPcmAudioFilter output) {
         return build(new JSONObject(data.toString()), format, output);
     }
@@ -34,11 +46,18 @@ public interface AudioFilterExtension {
     /**
      * @param data JSON data received from the client under the extension name key.
      * @return whether to build a filter. Returning false makes this extension do nothing.
+     *
+     * @deprecated As of v3.7 Jackson is the preferred way of JSON serialization,
+     * use {@link AudioFilterExtension#isEnabled(JsonNode)} instead.
      */
     default boolean isEnabled(JSONObject data) {
         return false;
     }
 
+    /**
+     * @param data JSON data received from the client under the extension name key.
+     * @return whether to build a filter. Returning false makes this extension do nothing.
+     */
     default boolean isEnabled(JsonNode data) {
         return isEnabled(new JSONObject(data.toString()));
     }
