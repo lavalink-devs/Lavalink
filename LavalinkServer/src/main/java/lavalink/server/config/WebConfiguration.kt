bbfb41d5caa52aaf26d43ffdb6b95a4ae97e2a8b
TopiSenpai <tobi@strasserhof.com>
2022-11-08 20:22:45 +0100

    automatically register HandlerInterceptor for plugins to intercept rest endpoints

--- LavalinkServer/src/main/java/lavalink/server/config/WebConfiguration.kt
+++ LavalinkServer/src/main/java/lavalink/server/config/WebConfiguration.kt
@@ -6,19 +6,25 @@ import org.springframework.http.converter.HttpMessageConverter
 import org.springframework.http.converter.StringHttpMessageConverter
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
+import org.springframework.web.servlet.HandlerInterceptor
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 
 @Configuration
 @EnableWebMvc
-class WebConfiguration : WebMvcConfigurer {
+class WebConfiguration(private val interceptors: List<HandlerInterceptor>) : WebMvcConfigurer {
 
     override fun configureMessageConverters(converters: MutableList<HttpMessageConverter<*>>) {
         val builder = Jackson2ObjectMapperBuilder()
         builder.configure(objectMapper())
         converters.add(StringHttpMessageConverter())
         converters.add(MappingJackson2HttpMessageConverter(builder.build()))
+    }
+
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        interceptors.forEach { registry.addInterceptor(it) }
     }
 
 }
