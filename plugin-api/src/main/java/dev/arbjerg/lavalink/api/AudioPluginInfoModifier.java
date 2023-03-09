295ee53394ae64202b7d089d02200f11095d4612
Michael Rittmeister <michael@rittmeister.in>
2023-03-09 12:53:37 +0100

    Make AudioPluginInfoModifier return new JsonElement

--- plugin-api/src/main/java/dev/arbjerg/lavalink/api/AudioPluginInfoModifier.java
+++ plugin-api/src/main/java/dev/arbjerg/lavalink/api/AudioPluginInfoModifier.java
@@ -3,6 +3,8 @@ package dev.arbjerg.lavalink.api;
 import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import kotlinx.serialization.json.JsonObject;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public interface AudioPluginInfoModifier {
 
@@ -10,18 +12,21 @@ public interface AudioPluginInfoModifier {
      * Adds custom fields to an {@link AudioTrack}'s JSON.
      *
      * @param track the track that was loaded.
-     * @param node  the info node which can be altered.
+     * @return an {@link JsonObject} containing customized info
      */
-    default void modifyAudioTrackPluginInfo(AudioTrack track, JsonObject node) {
+    @Nullable
+    default JsonObject modifyAudioTrackPluginInfo(@NotNull AudioTrack track) {
+        return null;
     }
 
     /**
      * Adds custom fields to an {@link AudioPlaylist}'s JSON.
      *
      * @param playlist the playlist that was loaded.
-     * @param node     the info node which can be altered.
+     * @return an {@link JsonObject} containing customized info
      */
-    default void modifyAudioPlaylistPluginInfo(AudioPlaylist playlist, JsonObject node) {
+    @Nullable
+    default JsonObject modifyAudioPlaylistPluginInfo(@NotNull AudioPlaylist playlist) {
+        return null;
     }
-
 }
