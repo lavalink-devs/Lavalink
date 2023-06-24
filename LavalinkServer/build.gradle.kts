d87b5a371337649c9a7f48b23cc771c789062416
Toπ <git@topi.wtf>
2023-06-24 19:25:45 +0200

    add musl jar to artifacts & add missing or

--- LavalinkServer/build.gradle.kts
+++ LavalinkServer/build.gradle.kts
@@ -126,8 +126,7 @@ tasks {
             archiveFileName.set("Lavalink-musl.jar")
             // Exclude base dependency jar
             exclude {
-                it.name.contains("lavaplayer-natives-fork")
-                it.name.contains("udpqueue-native-")
+                it.name.contains("lavaplayer-natives-fork") || it.name.contains("udpqueue-native-")
             }
 
             // Add custom jar
