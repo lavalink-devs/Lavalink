ab6c327a8ff12db2dbec42909c8f5d965e8a4bd8
Toπ <git@topi.wtf>
2023-10-14 18:17:21 +0200

    remove unused import

--- protocol/src/commonTest/kotlin/LoadResultSerializerTest.kt
+++ protocol/src/commonTest/kotlin/LoadResultSerializerTest.kt
@@ -1,7 +1,6 @@
 import dev.arbjerg.lavalink.protocol.v4.Exception
 import dev.arbjerg.lavalink.protocol.v4.LoadResult
 import dev.arbjerg.lavalink.protocol.v4.ResultStatus
-import kotlinx.serialization.json.JsonObject
 import kotlin.js.JsName
 import kotlin.test.Test
 import kotlin.test.assertIs
