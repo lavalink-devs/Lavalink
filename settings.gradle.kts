380cfb5b9061034aa4a9d45d9c20e954592ef9a2
Toπ <git@topi.wtf>
2023-11-05 01:17:47 +0100

    update lavaplayer to 2.0.3

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -36,7 +36,7 @@ fun VersionCatalogBuilder.spring() {
 }
 
 fun VersionCatalogBuilder.voice() {
-    version("lavaplayer", "2.0.2")
+    version("lavaplayer", "2.0.3")
 
     library("lavaplayer",            "dev.arbjerg", "lavaplayer").versionRef("lavaplayer")
     library("lavaplayer-ip-rotator", "dev.arbjerg", "lavaplayer-ext-youtube-rotator").versionRef("lavaplayer")
