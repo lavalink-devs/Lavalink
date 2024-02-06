51d32f3955ba5bd15eee645724d271ea58ab0b46
devoxin <15076404+devoxin@users.noreply.github.com>
2024-02-06 15:38:30 +0000

    Update LavalinkServer/src/main/java/lavalink/server/bootstrap/PluginManager.kt
    Co-authored-by: Toπ <git@topi.wtf>

--- LavalinkServer/src/main/java/lavalink/server/bootstrap/PluginManager.kt
+++ LavalinkServer/src/main/java/lavalink/server/bootstrap/PluginManager.kt
@@ -69,7 +69,8 @@ class PluginManager(val config: PluginsConfig) {
 
                 // Delete versions of the plugin that aren't the same as declared version.
                 if (!jar.file.delete()) throw RuntimeException("Failed to delete ${jar.file.path}")
-                log.info("Deleted ${jar.file.path} (old version: ${jar.manifest.version})")
+                log.info("Deleted ${jar.file.path} (new version: ${declaration.version})")
+")
             }
 
             if (!hasCurrentVersion) {
