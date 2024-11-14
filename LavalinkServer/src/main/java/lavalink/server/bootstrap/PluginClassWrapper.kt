9c1a22c68a4a75a8e338964457168d18d738bb34
Michael Rittmeister <michael@rittmeister.in>
2024-11-14 02:09:57 +0100

    Load configurations from configurations.idx

--- LavalinkServer/src/main/java/lavalink/server/bootstrap/PluginClassWrapper.kt
+++ LavalinkServer/src/main/java/lavalink/server/bootstrap/PluginClassWrapper.kt
@@ -5,20 +5,25 @@ import org.pf4j.PluginFactory
 import org.pf4j.PluginWrapper
 import org.pf4j.spring.SpringPlugin
 import org.pf4j.spring.SpringPluginManager
+import org.pf4j.util.FileUtils
 import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.AnnotationConfigApplicationContext
+import kotlin.io.path.useLines
 
-class PluginClassWrapper(@get:JvmName("getLavalinkContext") val wrapper: PluginWrapper) : SpringPlugin(wrapper) {
+class PluginClassWrapper(val context: PluginWrapper) : SpringPlugin(context) {
     override fun createApplicationContext(): ApplicationContext {
-        val parent = (wrapper.pluginManager as SpringPluginManager).applicationContext
+        val parent = (context.pluginManager as SpringPluginManager).applicationContext
         return AnnotationConfigApplicationContext().apply {
             this.parent = parent
-            classLoader = wrapper.pluginClassLoader
-            (wrapper.descriptor as LavalinkPluginDescriptor).springConfigurationFiles.forEach {
-                log.debug("Registering configuration {} from plugin {}", it, wrapper.pluginId)
-                val clazz = wrapper.pluginClassLoader.loadClass(it)
-                register(clazz)
-            }
+            classLoader = context.pluginClassLoader
+            FileUtils.getPath(context.pluginPath, "META-INF", "configurations.idx")
+                .useLines {
+                    it.forEach { className ->
+                        log.debug("Registering configuration {} from plugin {}", className, context.pluginId)
+                        val clazz = context.pluginClassLoader.loadClass(className)
+                        register(clazz)
+                    }
+                }
             refresh()
         }
     }
