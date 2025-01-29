eaac1924a9ca8e36b58c6558b759b7f7664b4ef2
topi314 <git@topi.wtf>
2025-01-29 14:24:04 +0100

    return full stacktrace as cause in loadtrack endpoint

--- LavalinkServer/src/main/java/lavalink/server/util/util.kt
+++ LavalinkServer/src/main/java/lavalink/server/util/util.kt
@@ -154,7 +154,7 @@ fun FriendlyException.Severity.toLavalink() = when (this) {
 fun Exception.Companion.fromFriendlyException(e: FriendlyException) = Exception(
     e.message,
     Exception.Severity.fromFriendlyException(e.severity),
-    e.toString()
+    e.stackTraceToString()
 )
 
 fun AudioTrackEndReason.toLavalink() = when (this) {
