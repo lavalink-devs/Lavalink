43e44a4e23bbb1fcba2049b0de4350cf61b35dc7
topi314 <git@topi.wtf>
2025-05-30 21:20:52 +0200

    refactor: lavalink metrics collect code

--- LavalinkServer/src/main/java/lavalink/server/metrics/LavalinkStatsCollector.kt
+++ LavalinkServer/src/main/java/lavalink/server/metrics/LavalinkStatsCollector.kt
@@ -6,7 +6,6 @@ import lavalink.server.io.StatsCollector
 import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.stereotype.Component
-import java.util.*
 
 @Component
 @ConditionalOnProperty("metrics.prometheus.enabled") // Only create this if prometheus is enabled
@@ -45,109 +44,78 @@ class LavalinkStatsCollector(
     }
 
     override fun collect(): MutableList<MetricFamilySamples> {
-        val mfs = mutableListOf<MetricFamilySamples>()
-
         // Since we don't have a context, framestats will be null and we can ignore them
         val stats = statsProvider.retrieveStats(null)
-
-        val playersGauge = GaugeMetricFamily(
-            PLAYERS_METRIC_NAME,
-            PLAYERS_HELP,
-            mutableListOf<String?>()
-        )
-        playersGauge.addMetric(mutableListOf<String?>(), stats.players.toDouble())
-        mfs.add(playersGauge)
-
-        val playingPlayersGauge = GaugeMetricFamily(
-            PLAYING_PLAYERS_METRIC_NAME,
-            PLAYING_PLAYERS_HELP,
-            mutableListOf<String?>()
-        )
-        playingPlayersGauge.addMetric(mutableListOf<String?>(), stats.playingPlayers.toDouble())
-        mfs.add(playingPlayersGauge)
-
-        val uptimeGauge = GaugeMetricFamily(
-            UPTIME_METRIC_NAME,
-            UPTIME_HELP,
-            mutableListOf<String?>()
-        )
-        uptimeGauge.addMetric(mutableListOf<String?>(), stats.uptime.toDouble())
-        mfs.add(uptimeGauge)
-
         val memory = stats.memory
-        val memFreeGauge = GaugeMetricFamily(
-            MEMORY_FREE_METRIC_NAME,
-            "$MEMORY_HELP (Free)",
-            mutableListOf<String?>()
-        )
-        memFreeGauge.addMetric(mutableListOf<String?>(), memory.free.toDouble())
-        mfs.add(memFreeGauge)
-
-        val memUsedGauge = GaugeMetricFamily(
-            MEMORY_USED_METRIC_NAME,
-            "$MEMORY_HELP (Used)",
-            mutableListOf<String?>()
-        )
-        memUsedGauge.addMetric(mutableListOf<String?>(), memory.used.toDouble())
-        mfs.add(memUsedGauge)
-
-        val memAllocatedGauge = GaugeMetricFamily(
-            MEMORY_ALLOCATED_METRIC_NAME,
-            "$MEMORY_HELP (Allocated)",
-            mutableListOf<String?>()
-        )
-        memAllocatedGauge.addMetric(mutableListOf<String?>(), memory.allocated.toDouble())
-        mfs.add(memAllocatedGauge)
-
-        val memReservableGauge = GaugeMetricFamily(
-            MEMORY_RESERVABLE_METRIC_NAME,
-            "$MEMORY_HELP (Reservable)",
-            mutableListOf<String?>()
-        )
-        memReservableGauge.addMetric(mutableListOf<String?>(), memory.reservable.toDouble())
-        mfs.add(memReservableGauge)
-
         val cpu = stats.cpu
-        val cpuCoresGauge = GaugeMetricFamily(
-            CPU_CORES_METRIC_NAME,
-            "$CPU_HELP (Cores)",
-            mutableListOf<String?>()
-        )
-        cpuCoresGauge.addMetric(mutableListOf<String?>(), cpu.cores.toDouble())
-        mfs.add(cpuCoresGauge)
 
-        val cpuSystemLoadGauge = GaugeMetricFamily(
-            CPU_SYSTEM_LOAD_METRIC_NAME,
-            "$CPU_HELP (System Load)",
-            mutableListOf<String?>()
+        return mutableListOf(
+            GaugeMetricFamily(
+                PLAYERS_METRIC_NAME,
+                PLAYERS_HELP,
+                stats.players.toDouble()
+            ),
+            GaugeMetricFamily(
+                PLAYING_PLAYERS_METRIC_NAME,
+                PLAYING_PLAYERS_HELP,
+                stats.playingPlayers.toDouble()
+            ),
+            GaugeMetricFamily(
+                UPTIME_METRIC_NAME,
+                UPTIME_HELP,
+                stats.uptime.toDouble()
+            ),
+            GaugeMetricFamily(
+                MEMORY_FREE_METRIC_NAME,
+                "$MEMORY_HELP (Free)",
+                memory.free.toDouble()
+            ),
+            GaugeMetricFamily(
+                MEMORY_USED_METRIC_NAME,
+                "$MEMORY_HELP (Used)",
+                memory.used.toDouble()
+            ),
+            GaugeMetricFamily(
+                MEMORY_ALLOCATED_METRIC_NAME,
+                "$MEMORY_HELP (Allocated)",
+                memory.allocated.toDouble()
+            ),
+            GaugeMetricFamily(
+                MEMORY_RESERVABLE_METRIC_NAME,
+                "$MEMORY_HELP (Reservable)",
+                memory.reservable.toDouble()
+            ),
+            GaugeMetricFamily(
+                CPU_CORES_METRIC_NAME,
+                "$CPU_HELP (Cores)",
+                cpu.cores.toDouble()
+            ),
+            GaugeMetricFamily(
+                CPU_SYSTEM_LOAD_METRIC_NAME,
+                "$CPU_HELP (System Load)",
+                cpu.systemLoad
+            ),
+            GaugeMetricFamily(
+                CPU_LAVALINK_LOAD_METRIC_NAME,
+                "$CPU_HELP (LL Load)",
+                cpu.lavalinkLoad
+            )
         )
-        cpuSystemLoadGauge.addMetric(mutableListOf<String?>(), cpu.systemLoad)
-        mfs.add(cpuSystemLoadGauge)
-
-        val cpuLavalinkLoadGauge = GaugeMetricFamily(
-            CPU_LAVALINK_LOAD_METRIC_NAME,
-            "$CPU_HELP (LL Load)",
-            mutableListOf<String?>()
-        )
-        cpuLavalinkLoadGauge.addMetric(mutableListOf<String?>(), cpu.lavalinkLoad)
-        mfs.add(cpuLavalinkLoadGauge)
-
-        return mfs
     }
 
     override fun describe(): MutableList<MetricFamilySamples> {
         // Used by prometheus for validation
         return mutableListOf(
-            GaugeMetricFamily(PLAYERS_METRIC_NAME, PLAYERS_HELP, mutableListOf<String?>()),
-            GaugeMetricFamily(PLAYING_PLAYERS_METRIC_NAME, PLAYING_PLAYERS_HELP, mutableListOf<String?>()),
-            GaugeMetricFamily(UPTIME_METRIC_NAME, UPTIME_HELP, mutableListOf<String?>()),
-            GaugeMetricFamily(MEMORY_FREE_METRIC_NAME, "$MEMORY_HELP (Free)", mutableListOf<String?>()),
-            GaugeMetricFamily(MEMORY_USED_METRIC_NAME, "$MEMORY_HELP (Used)", mutableListOf<String?>()),
-            GaugeMetricFamily(MEMORY_ALLOCATED_METRIC_NAME, "$MEMORY_HELP (Allocated)", mutableListOf<String?>()),
-            GaugeMetricFamily(MEMORY_RESERVABLE_METRIC_NAME, "$MEMORY_HELP (Reservable)", mutableListOf<String?>()),
-            GaugeMetricFamily(CPU_CORES_METRIC_NAME, "$CPU_HELP (Cores)", mutableListOf<String?>()),
-            GaugeMetricFamily(CPU_SYSTEM_LOAD_METRIC_NAME, "$CPU_HELP (System Load)", mutableListOf<String?>()),
-            GaugeMetricFamily(CPU_LAVALINK_LOAD_METRIC_NAME, "$CPU_HELP (Lavalink Load)", mutableListOf<String?>())
+            GaugeMetricFamily(PLAYERS_METRIC_NAME, PLAYERS_HELP, mutableListOf()),
+            GaugeMetricFamily(PLAYING_PLAYERS_METRIC_NAME, PLAYING_PLAYERS_HELP, mutableListOf()),
+            GaugeMetricFamily(UPTIME_METRIC_NAME, UPTIME_HELP, mutableListOf()),
+            GaugeMetricFamily(MEMORY_FREE_METRIC_NAME, "$MEMORY_HELP (Free)", mutableListOf()),
+            GaugeMetricFamily(MEMORY_USED_METRIC_NAME, "$MEMORY_HELP (Used)", mutableListOf()),
+            GaugeMetricFamily(MEMORY_ALLOCATED_METRIC_NAME, "$MEMORY_HELP (Allocated)", mutableListOf()),
+            GaugeMetricFamily(MEMORY_RESERVABLE_METRIC_NAME, "$MEMORY_HELP (Reservable)", mutableListOf()),
+            GaugeMetricFamily(CPU_CORES_METRIC_NAME, "$CPU_HELP (Cores)", mutableListOf()),
+            GaugeMetricFamily(CPU_SYSTEM_LOAD_METRIC_NAME, "$CPU_HELP (System Load)", mutableListOf()),
+            GaugeMetricFamily(CPU_LAVALINK_LOAD_METRIC_NAME, "$CPU_HELP (Lavalink Load)", mutableListOf())
         )
     }
 }
